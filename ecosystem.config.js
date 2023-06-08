3e8d19b4bffb8a2cd055a330bdfee89c3d7ed40c
João <joaovictor.ramos34@gmail.com>
2023-06-08 18:56:38 -0300

    Configurando o arquivo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  apps : [{
-    name: "app",
-    script: "./app.js",
-    env: {
-      NODE_ENV: "development",
+  apps: [
+    {
+      name: "app",
+      script: "./app.js",
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
-    env_production: {
-      NODE_ENV: "production",
-    }
-  }, {
-     name: 'worker',
-     script: 'worker.js'
-  }]
-}
+  ],
+};
