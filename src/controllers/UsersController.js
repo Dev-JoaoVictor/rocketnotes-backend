e149c5568379172caec8e50b3bf846ac5233b5b7
João <joaovictor.ramos34@gmail.com>
2023-06-10 12:47:18 -0300

    utilizando o User Repository

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,8 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+
 const sqliteConnection = require("../database/sqlite");
+const UserRepository = require("../repositories/UserRepository");
 
 class UsersController {
   /** O controller poder ter no máximo 5 métodos
@@ -13,11 +15,9 @@ class UsersController {
   async create(resquest, response) {
     const { name, email, password } = resquest.body;
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get(
-      "SELECT * FROM  users WHERE email = (?)",
-      [email]
-    ); // subtitui pela variável
+    const userRepository = new UserRepository();
+
+    const checkUserExists = await userRepository.findByEmail(email);
 
     if (checkUserExists) {
       throw new AppError("Este e-mail já está em uso.");
@@ -25,20 +25,19 @@ class UsersController {
 
     const hashedPassword = await hash(password, 8);
 
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, hashedPassword]
-    );
+    await userRepository.create({ name, email, password: hashedPassword });
 
     return response.status(201).json();
   }
 
   async update(request, response) {
     const { name, email, password, old_password } = request.body;
-    const user_id = request.user.id
+    const user_id = request.user.id;
 
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ]);
 
     if (!user) {
       throw new AppError("Usuário não encontrado.");
