69e0350c712fc8219201c0d979ccfec9605f6df5
João <joaovictor.ramos34@gmail.com>
2023-06-12 12:01:24 -0300

    Criando um grupo de testes

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,18 +1,22 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
 
-it("user should be create", async () => {
-  const user = {
-    name: "User test",
-    email: "user@test.com",
-    password: "123",
-  };
+describe("UserCreateService", () => {
+  it("user should be create", async () => {
+    const user = {
+      name: "User test",
+      email: "user@test.com",
+      password: "123",
+    };
 
-  const userRepositoryInMemory = new UserRepositoryInMemory();
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    const userCreated = await userCreateService.execute(user);
 
-  console.log(userCreated)
+    console.log(userCreated);
 
-  expect(userCreated).toHaveProperty("id");
+    expect(userCreated).toHaveProperty("id");
+  });
+
+  
 });
