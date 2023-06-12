503123b030f48dc5a3870bee1fac06393e32726c
João <joaovictor.ramos34@gmail.com>
2023-06-12 14:18:59 -0300

    utilizando o beforeEach

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -3,6 +3,14 @@ const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
 const AppError = require("../utils/AppError");
 
 describe("UserCreateService", () => {
+  let userRepositoryInMemory = null;
+  let userCreateService = null;
+
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory();
+    userCreateService = new UserCreateService(userRepositoryInMemory);
+  });
+
   it("user should be create", async () => {
     const user = {
       name: "User test",
@@ -10,8 +18,6 @@ describe("UserCreateService", () => {
       password: "123",
     };
 
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
     const userCreated = await userCreateService.execute(user);
 
     console.log(userCreated);
@@ -32,10 +38,9 @@ describe("UserCreateService", () => {
       password: "456",
     };
 
-    const userRepository = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepository);
-
     await userCreateService.execute(user1);
-    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso."));
+    await expect(userCreateService.execute(user2)).rejects.toEqual(
+      new AppError("Este e-mail já está em uso.")
+    );
   });
 });
